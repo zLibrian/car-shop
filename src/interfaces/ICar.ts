c964dcc4cd71b1c1b2533d74cc1127569bb4a06c
Kevin Willian <kevinwillian084@gmail.com>
2022-10-10 19:41:09 -0300

    feat: Adiciona mensagens de erro customizadas à interface de ICar

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -1,9 +1,22 @@
 import z from 'zod';
 import { VehicleSchema } from './IVehicle';
 
-export const CarSchema = VehicleSchema.extend({
-  doorsQty: z.number().min(2).max(4).int(),
-  seatsQty: z.number().min(2).max(7).int(),
+export const CarZodSchema = VehicleSchema.extend({
+  doorsQty: z.number({
+    required_error: 'Insira número de portas do carro',
+    invalid_type_error: 'Número de portas do carro deve ser um número',
+  })
+    .min(2, { message: 'Número de portas do carro deve ser maior ou igual a 2' })
+    .max(4, { message: 'Número de portas do carro deve ser menor ou igual a 4' })
+    .int({ message: 'Número de portas deve ser um número inteiro' }),
+
+  seatsQty: z.number({
+    required_error: 'Número de assentos do carro requerido',
+    invalid_type_error: 'Numero de assentos deve ser um número',
+  })
+    .min(2, { message: 'Número de assentos do carro deve ser maior ou igual a 2' })
+    .max(7, { message: 'Número de assentos do carro deve ser menor ou igual a 4' })
+    .int({ message: 'Número de assentos deve ser um valor inteiro' }),
 });
 
-export type ICar = z.infer<typeof CarSchema>;
+export type ICar = z.infer<typeof CarZodSchema>;
