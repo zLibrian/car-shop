44d24f0919d5b971217718844355fbb597c6dace
Kevin Willian <kevinwillian084@gmail.com>
2022-10-10 23:46:27 -0300

    feat: Adiciona metodo de leitura de todos os carros cadastrados na classe abstrata MongoModel

--- src/models/MongoModel.ts
+++ src/models/MongoModel.ts
@@ -10,4 +10,23 @@ export default abstract class MongoModel<T> implements IModel<T> {
   public async create(obj: T): Promise<T> {
     return this._model.create({ ...obj });
   }
+
+  public async read(): Promise<T[]> {
+    return this._model.find();
+  }
+
+  // public async readOne(_id: string): Promise<T | null> {
+  //   if (!isValidObjectId(_id)) throw new Error('InvalidMongoId');
+  //   return this._model.findOne({ _id });
+  // }
+
+  // public async update(_id: string, _obj: Partial<T>): Promise<T | null> {
+  //   if (!isValidObjectId(_id)) throw new Error('InvalidMongoId');
+  //   return this._model.findOneAndUpdate({ _id, _obj });
+  // }
+
+  // public async delete(_id: string): Promise<T | null> {
+  //   if (!isValidObjectId(_id)) throw new Error('InvalidMongoId');
+  //   return this._model.findOneAndDelete({ _id });
+  // }
 }
