6588e37d7bcab5b973b80077b701c7d53d8da6ce
Kevin Willian <kevinwillian084@gmail.com>
2022-10-10 21:00:46 -0300

    feat: Cria carRouter e depois o adiciona ao App

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,12 @@ import express from 'express';
 import 'express-async-errors';
 import handleError from './middleware/handleError';
 
+import CarRouter from './routes/carRouter';
+
 const app = express();
 
 app.use(express.json());
+app.use('/cars', CarRouter);
 app.use(handleError);
 
 export default app;
---
+++ src/routes/carRouter.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import CarController from '../controllers/CarController';
+import CarModel from '../models/CarModel';
+import CarService from '../services/CarService';
+
+const carRouter = Router();
+
+const carModel = new CarModel();
+const carService = new CarService(carModel);
+const carController = new CarController(carService);
+
+carRouter.post('/', (req, res) => carController.create(req, res));
+
+export default carRouter;
