ef893a78a935413ba17700a28bf709786e2e20b6
Kevin Willian <kevinwillian084@gmail.com>
2022-10-11 00:27:55 -0300

    feat: Adiciona nova validacao de error no middleware de error

--- src/middleware/handleError.ts
+++ src/middleware/handleError.ts
@@ -1,10 +1,17 @@
 import { NextFunction, Request, Response } from 'express';
 import { ZodError } from 'zod';
+import errorMessage from '../utils/errorMessages';
 
 function handleError(err: Error, _req: Request, res: Response, _next: NextFunction) {
   if (err instanceof ZodError) {
     return res.status(400).json({ error: err.issues });
   }
+
+  const customError = errorMessage[err.message];
+  if (customError) {
+    return res.status(Number(customError.code)).json({ error: customError.message });
+  }
+
   return res.status(500).json({ message: 'Internal server error' });
 }
 export default handleError;
