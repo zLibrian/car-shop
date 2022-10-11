4cdede155fa2454707eb3be5d0e5503808de90c0
Kevin Willian <kevinwillian084@gmail.com>
2022-10-10 23:45:57 -0300

    feat: Adiciona método de leitura de todos os carros cadastrados nas interfaces IModel e IService

--- src/interfaces/IService.ts
+++ src/interfaces/IService.ts
@@ -1,3 +1,6 @@
 export interface IService<T> {
   create(obj: unknown): Promise<T>,
+  read(): Promise<T[]>,
+  // readOne(str: string): Promise<T | null>,
+  // update(_id: string, obj: T): Promise<T | null>,
 }
